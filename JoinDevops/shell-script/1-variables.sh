ac0d580fbdaa4036478e7987a724c2f106584bfe
charan-creator <85110032+charan-creator@users.noreply.github.com>
2024-04-21 15:09:02 +0000

    adding variable

---
+++ JoinDevops/shell-script/1-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Charan: what are you doing myan ?"
+echo "Cherry: Nothing sir!, just trying to change my life "
+echo "charan: Oh! that's great, but how and what you are doing to have change in your life ?"
+echo "cherry: By upskiiling myself in Devops "
+echo "charan: oh great keep doing great work,will see you later, Bye......"
+
