8ffe496d6086cbef5903609f004e10b0c39ba4e2
charan-creator <85110032+charan-creator@users.noreply.github.com>
2024-04-22 02:24:25 +0000

    Modified Script

--- JoinDevops/expense-project-shell/mysql.sh
+++ JoinDevops/expense-project-shell/mysql.sh
@@ -1,53 +1,54 @@
 #!/bin/bash
 
 USERID=$(id -u)
+SCRIPT_NAME=$(echo $0 | cut -d "," -f1)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP
+#setting up color codes
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-echo "Please enter DB password:"
+
+echo "Please enter DB password: "
 read -s mysql_root_password
 
 VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2...$R FAILURE $N"
+    exit 1
     else
-        echo -e "$2...$G SUCCESS $N"
+    echo -e "$2...$G SUCCESS $N"
     fi
 }
 
 if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
+then 
+echo "Please run this script with root user access"
+exit 1
+else 
+echo "Cheers ! you are super user."
 fi
-
 
 dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Server"
+VALIDATE $? "Installing MySql Server"
 
 systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling MySQL Server"
+VALIDATE $? "Enabling MySql Server"
 
 systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting MySQL Server"
+VALIDATE $? "Starting MySql Server"
 
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIDATE $? "Setting up root password"
+#ExpenseApp@1
+# Below code for Idempotent nature
 
-#Below code will be useful for idempotent nature
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.nagacharan.site -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MySQL Root password Setup"
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+VALIDATE $? "MySql Root Password Setup"
 else
-    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+echo -e "MYSQL Root Password is already Setup...$Y SKIPPING $N"
 fi
+
