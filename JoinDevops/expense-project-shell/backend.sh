6b8b22f0ac1844d29836f925f11c98fea25119be
charan-creator <85110032+charan-creator@users.noreply.github.com>
2024-04-22 03:13:37 +0000

    backend script added

---
+++ JoinDevops/expense-project-shell/backend.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+USERID=$(id -u)
+SCRIPT_NAME=$(echo $0 | cut -d "," -f1)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP
+#setting up color codes
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2...$R FAILURE $N"
+    exit 1
+    else
+    echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+echo "Please run this script with root user access"
+exit 1
+else 
+echo "Cheers ! you are super user."
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling default nodejs module"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs:20 module"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing Nodejs"
+
+id expense &>>$LOGFILE
+if [ $id -ne 0 ]
+then 
+ useradd expense &>>$LOGFILE
+ VALIDATE $? "Creating expense user"
+else
+echo -e "expense user already created..$Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "creating app directory"
+
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs Dependencies"
+
+#check your repo and path
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service"
+
+
